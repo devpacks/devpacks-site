e205d96817a744357fe359db73be63fde87dcc9a
bitc <996487+bitc@users.noreply.github.com>
2025-01-01 00:00:00 +0000

    Add github action deploy workflow

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+GHC=$(devpacks pkg-path ghc -p ghc)
+CABAL=$(devpacks pkg-path haskell_cabal_install -p cabal)
+PKG_CONFIG=$(devpacks pkg-path pkgconfig -p pkg-config)
+
+ZLIB_PATH=$(devpacks pkg-path zlib)
+
+"$GHC" --version
+"$CABAL" --version
+
+export PKG_CONFIG_PATH="${ZLIB_PATH}"/lib/pkgconfig
+
+"$CABAL" update
+"$CABAL" --with-ghc="$GHC" --with-pkg-config="$PKG_CONFIG" run site -- build
